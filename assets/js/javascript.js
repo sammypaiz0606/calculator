5f99d53e201c34c92eb0d68e584507acd75fae59
sammy paiz <sammypaiz@sammys-MacBook-Pro.local>
2019-11-20 14:28:48 -0800

    click button & alert box

---
+++ assets/js/javascript.js
@@ -0,0 +1,8 @@
+var ctbutton = document.getElementsByClassName('cbutton');
+console.log(ctbutton);
+
+for(var i=0; i <ctbutton.length; i++) {
+ctbutton[i].addEventListener('click', function() {
+    alert('clicked');
+    });
+}
