b7a7cc7304bdc1d6c9ff5a0fd13a7e29dc759199
sammy paiz <sammypaiz@sammys-MacBook-Pro.local>
2019-11-20 15:20:37 -0800

    added output value when clicked

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,9 +1,14 @@
-// var ctbutton = document.getElementsByClassName('cbutton');
-// console.log(ctbutton);
 
-// for(var i=0; i <ctbutton.length; i++) {
-// ctbutton[i].addEventListener('click', function() {
-//     alert('clicked');
-//     });
-// }
-
+var mybtn = document.getElementsByClassName('cbutton');
+var myOutput = document.getElementById('output');
+var myCal = '';
+console.log(mybtn);
+for(var i=0; i < mybtn.length; i++) {
+    
+    mybtn[i].addEventListener('click', function() {
+        var myValue = this.innerHTML; 
+        myCal = myCal + myValue;
+        myOutput.innerHTML = myCal;
+        console.log('clicked');
+    });
+} 
