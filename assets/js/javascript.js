7735dac8c8fb573f825acadff85233012e7bc32d
sammy paiz <sammypaiz@sammys-MacBook-Pro.local>
2019-11-20 14:48:58 -0800

    added more buttons

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,9 @@
-var ctbutton = document.getElementsByClassName('cbutton');
-console.log(ctbutton);
+// var ctbutton = document.getElementsByClassName('cbutton');
+// console.log(ctbutton);
 
-for(var i=0; i <ctbutton.length; i++) {
-ctbutton[i].addEventListener('click', function() {
-    alert('clicked');
-    });
-}
+// for(var i=0; i <ctbutton.length; i++) {
+// ctbutton[i].addEventListener('click', function() {
+//     alert('clicked');
+//     });
+// }
+
