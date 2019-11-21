a0eba87b29910989f559aaa9bfababe7bd2219fa
sammy paiz <sammypaiz@sammys-MacBook-Pro.local>
2019-11-20 23:28:42 -0800

    if(), boolean, & OR operator

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,13 +2,39 @@
 var mybtn = document.getElementsByClassName('cbutton');
 var myOutput = document.getElementById('output');
 var myCal = '';
-console.log(mybtn);
+var myCom = false;
+var mySwitch = false;
+
+// console.log(mybtn);
 for(var i=0; i < mybtn.length; i++) {
     
     mybtn[i].addEventListener('click', function() {
         var myValue = this.innerHTML; 
-        myCal = myCal + myValue;
+        if(myCom || myCal =='0') {
+            myCom = false;
+            myCal = '';
+        }
+
+
+        if(myValue == '=') {
+            myCal = eval(myCal);
+            myCom = true;
+         }else if(myValue == 'C') {
+            myCal = 0;
+            myCom = true;
+        }else{
+            myCal += myValue;
+        }
+
+        if(mySwitch) {
+            mySwitch = false;
+            myCal = eval(myCal);
+
+        }
+
+        if(myValue == '+' || myValue == '-' || myValue =='*' || myValue == '/') {
+            mySwitch = true;
+        }
         myOutput.innerHTML = myCal;
-        console.log('clicked');
     });
 } 
